aac710a19c59b746eec584ecb99c61b024a8e4fa
nazartsap21 <nazar.tsap.ua@gmail.com>
2025-09-26 19:10:43 +0300

    feat: add dockerfile

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from src import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+
